1e96bc1e49ba9acc40270e1b4592c8890822a49a
Guillermo Alvarez <guillermoalvma@gmail.com>
2021-04-02 09:38:57 +0200

    first commit

--- src/components/HeaderSearchBar.tsx
+++ src/components/HeaderSearchBar.tsx
@@ -5,14 +5,14 @@ import {
   PlasmicHeaderSearchBar,
   DefaultHeaderSearchBarProps
 } from "./plasmic/brands_beats/PlasmicHeaderSearchBar";
-//import algoliasearch from 'algoliasearch';
-//import {InstantSearch, connectSearchBox} from 'react-instantsearch-dom';
+import algoliasearch from 'algoliasearch';
+import {InstantSearch} from 'react-instantsearch-dom';
 //import SearchContext from './SearchContext'
-/*
+
 const searchClient = algoliasearch(
   'E5ACT1VI4D',
   '58c5723bb97942db9b754bf244b1da75'
-);*/
+);
 
 // Your component props start with props for variants and slots you defined
 // in Plasmic, but you can add more here, like event handlers that you can
@@ -30,54 +30,7 @@ const searchClient = algoliasearch(
 interface HeaderSearchBarProps extends DefaultHeaderSearchBarProps {}
 
 function HeaderSearchBar(props: HeaderSearchBarProps) {
-  //const { status, togglePlayPause } = React.useContext(SearchContext)
-  const [focus, setFocus ] = React.useState(false)
-  const componentRef = React.useRef<any>(null);
-  const node = React.useRef<any>();
-
-  React.useEffect(() => {
-    
-    function handleClick(e:  MouseEvent): void{
-      if (componentRef.current){
-      if (node.current.contains(e.target) || componentRef.current.contains(e.target)) {
-        return;
-      }
-      setFocus(!focus)
-    }
-    };
-    console.log(handleClick)
-    // add when mounted
-    //document.addEventListener("mousedown", handleClick);
-    // return function to be called when unmounted
-    return () => {
-      //document.removeEventListener("mousedown", handleClick);
-    };
-  });
-  /*
-  function Tt({ hits, currentRefinement, refine }:any){
-    if(currentRefinement !== ''){
-      return (
-        <ul ref={componentRef} onClick={(e)=> console.log(e)}style={{zIndex:2, display:"block", top: "50px", position: "absolute", backgroundColor: "white", width: "80%", marginTop: "0px"}}>
-          {hits.slice(0,5).map((hit: any) => (
-              <li onClick={()=>togglePlayPause(hit.tags[0])} key={hit.objectID}>{hit.tags[0]}</li>
-          ))}
-          </ul>
-      )}
-    else{
-        return null
-    }
-  }*/
-
-  //const CustomAutocomplete = connectAutoComplete(Tt);
-  /*
-  const SearchBox = ({ currentRefinement, isSearchStalled, refine }:any) => (
-    <PlasmicHeaderSearchBar
-    
-    />
-  );
-  const CustomSearchBox = connectSearchBox(SearchBox);
-    */
-  return <PlasmicHeaderSearchBar/>
+  return <InstantSearch searchClient={searchClient} indexName="brands_beats"><PlasmicHeaderSearchBar/></InstantSearch>;
 }
 
 export default HeaderSearchBar;
